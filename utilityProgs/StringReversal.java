bf30583032d8837488a6274add8a43abbe8bb069
hchawla1991 <harshitchawla1991@yahoo.com>
2017-07-01 23:51:28 +0530

    core

---
+++ utilityProgs/StringReversal.java
@@ -0,0 +1,35 @@
+package utilityProgs;
+
+public class StringReversal {
+	
+	
+	
+	
+ private static String aaaa(String text){
+	 String[] NewText= text.split(" ");
+	 String temp=null;
+	 for(int i=NewText.length-1;i>=0;i--){
+		 
+		 temp+= NewText[i] + " ";
+	 }
+	 
+	return temp;
+	 
+ }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "0123456789 abcd mnbvc qwertyui nkiopl";
+		char[] aa = a.toCharArray();
+		String b = null;
+		String c;
+		for (int i = aa.length - 1; i >= 0; i--) {
+			b += aa[i];
+		}
+		//System.out.println("basic string reversal" + b);
+
+		System.out.println(aaaa(a));
+	
+	}
+
+}
