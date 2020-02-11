0bd227515bb0509bacb0312799f0ecb89c02c13b
harshit <harshit.chawla@soprasteria.com>
2020-02-11 10:04:36 +0530

    new

--- dataStructures/src/linkedList/Runner.java
+++ dataStructures/src/linkedList/Runner.java
@@ -1,22 +1,22 @@
 package linkedList;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Runner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		LinkedList list = new LinkedList();
-		list.insert(5);
-		list.insert(12);
-		list.insert(7);
-		list.insertAtStart(0);
-		list.insertAt(0, 90);
-		list.show();
-		list.deleteAt(0);
-		System.out.println("-----");
-		list.show();
-		list.deleteAt(2);
-		System.out.println("-----");
-		list.show();
+		/*
+		 * LinkedList list = new LinkedList(); list.insert(5); list.insert(12);
+		 * list.insert(7); list.insertAtStart(0); list.insertAt(0, 90); list.show();
+		 * list.deleteAt(0); System.out.println("-----"); list.show(); list.deleteAt(2);
+		 * System.out.println("-----"); list.show();
+		 */
+		
+		  int count[] = {34, 22,10,60,30,22};
+	      Set<Integer> set = new HashSet<Integer>();
+	      System.out.println(set.contains(1));
 	}
 
 }
